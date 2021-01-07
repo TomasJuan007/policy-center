e85eb438f81a76d2b7c3d4b9551a9b430c00cec1
Tomas <18819451598@163.com>
2021-01-07 21:02:02 +0800

    Spring Initializr

---
+++ src/test/java/com/github/tomasjuan007/policycenter/PolicyCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tomasjuan007.policycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
